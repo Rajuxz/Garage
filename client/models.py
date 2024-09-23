a9d0589699f473ecbbad7c51d5ab1e74efffa19f
Rajuxz <rajankhanalxz@gmail.com>
2024-09-23 23:17:08 +0545

    remove: password field

--- client/models.py
+++ client/models.py
@@ -7,6 +7,7 @@ class UserModel(AbstractBaseUser,PermissionsMixin):
     id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     user_name = models.CharField(verbose_name='User Name',blank=False,max_length=50)
     email_address = models.EmailField(verbose_name='Email Address',blank=False,unique=True)
+    phone_number = models.CharField(verbose_name="Phone Number",blank=True,max_length=15)
     latitude = models.FloatField(verbose_name='Latitude',default=0.0)
     longitude = models.FloatField(verbose_name='Longitude',default=0.0)
     created_at = models.DateTimeField(auto_now_add=True)
