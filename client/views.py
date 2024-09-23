e20e2a03743b89ac63a5da8231376aa08df8efaa
Rajuxz <rajankhanalxz@gmail.com>
2024-09-23 23:17:57 +0545

    render: register

--- client/views.py
+++ client/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .forms import RegisterForm
 # Create your views here.
 
 def home(request):
     return render(request,'home.html')
+
+def register(request):
+    form = RegisterForm()
+    return render(request, 'register.html',{'form':form})
 
 def test(request):
     return render(request,"test.html")
