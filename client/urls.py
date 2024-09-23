c78ff04692cc030d2e7f66ee0adf39a0c4115c15
Rajuxz <rajankhanalxz@gmail.com>
2024-09-23 23:17:24 +0545

    conf: url configuration for register page

--- client/urls.py
+++ client/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import home,test
+from .views import home,test,register
 urlpatterns = [
     path('',home, name="home"),
+    path('register/',register,name='register'),
     path('test/', test,name="test")
 ]
