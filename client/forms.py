a327944e10513efd8588c5502d53e852770d2bb0
Rajuxz <rajankhanalxz@gmail.com>
2024-09-27 23:59:19 +0545

    form: django registration form

--- client/forms.py
+++ client/forms.py
@@ -4,16 +4,13 @@ from .models import UserModel
 class RegisterForm(forms.ModelForm):
     class Meta:
        model = UserModel
-       fields = ['user_name','phone_number','email_address','password']
+       fields = ['user_name','phone_number','email_address','password','latitude','longitude']
        widgets = {
-            'user_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
-            'phone_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'}),
-            'email_address': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}),
-            'password': forms.PasswordInput(attrs={'class': 'form-control ', 'placeholder': 'Password'}),
+            'user_name': forms.TextInput(attrs={'class': 'form-control border border-black', 'placeholder': 'Username'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control border border-black', 'placeholder': 'Phone Number'}),
+            'email_address': forms.EmailInput(attrs={'class': 'form-control border border-black', 'placeholder': 'Email Address'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control  border border-black', 'placeholder': 'Password'}),
+            'latitude':forms.HiddenInput(),
+            'longitude':forms.HiddenInput()
         }
    
-    #Overriding form field initialization.
-    # def __init__(self,*args,**kwargs) -> None:
-    #     super(RegisterForm,self).__init__(*args,**kwargs)
-    #     for field in self.fields.values():
-    #         field.widget.attrs.update({'class':'form-control form-floating'})
