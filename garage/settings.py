6a56ea5da330323f432ffd0276ce42a95af58815
Rajuxz <rajankhanalxz@gmail.com>
2024-09-29 22:02:00 +0545

    add: login route and authentication backends

--- garage/settings.py
+++ garage/settings.py
@@ -122,3 +122,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login/'
+AUTHENTICATION_BACKENDS = [
+    'garage.authentication.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend'
+]
