5e562c7697e9728120e998e21344fd79f7782e82
Rajuxz <rajankhanalxz@gmail.com>
2024-09-29 22:00:49 +0545

    urls: for register page

--- client/urls.py
+++ client/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import home,test,register
+from . import views
 urlpatterns = [
-    path('',home, name="home"),
-    path('register/',register,name='register'),
-    path('test/', test,name="test")
+    path('',views.home, name="home"),
+    path('register/',views.register,name='register'),
+    path('client-dashboard/',views.client_dashboard,name="client-dashboard"),
+    path('test/', views.test,name="test")
 ]
