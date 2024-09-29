c4946bc083608879d8862a08ed852916da49e5e8
Rajuxz <rajankhanalxz@gmail.com>
2024-09-29 22:02:16 +0545

    add: login form template

--- garage/urls.py
+++ garage/urls.py
@@ -2,8 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('client.urls'),name='user')
+    path('',include('client.urls'),name='user'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
