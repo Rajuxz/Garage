b3ef3a6f07d6ec9276aa13a574b245df4f31659a
Rajuxz <rajankhanalxz@gmail.com>
2024-10-03 16:37:25 +0545

    form: for handeling login

--- garage/forms.py
+++ garage/forms.py
@@ -1,10 +1,40 @@
 from django import forms
-class LoginForm(forms.Form):
-    email = forms.EmailInput(required = True,attrs={'placeholder':'Email Address'})
-    password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    def clean(self):
-        cleaned_data = super().clean() #parent's clean method.
-        email = cleaned_data.get('email')
-        password = cleaned_data.get('password')
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+class LoginForm(AuthenticationForm):
+    username = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'Email Address', 'class': 'form-control'})
+    )
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'placeholder': 'Password','class':'form-control'}
+        ))
 
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+        print(f'cleaned data: {cleaned_data}')
+        print(username,password)
+        if username and password:
+            user = authenticate(username = username, password = password)
+            print(f'forms.py: {user}')
+            if user is None:
+                #This portion will check and raise errors following errors:
+                # If  user exists but authentication fails, that means password is incorrect.
+                # If no, email is not registered.
+                from django.contrib.auth import get_user_model
+                UserModel = get_user_model()
+                if UserModel.objects.filter(email_address = username).exists():
+                    print("Forms.py: Password mismatch.")
+                    self.add_error('password',f'Password does not matched')
+                else:
+                    print("Forms.py: Invalid email.")
+                    self.add_error('username','Invalid email address.')
         return cleaned_data
+    
+
+    def confirm_login_allowed(self, user):
+        # This method can be used to raise errors for specific conditions.
+        if not user.is_active:
+            raise ValidationError('This account is inactive.')
