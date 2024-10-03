aa359cefabd342bd1bd7fae303121dc6a9265096
Rajuxz <rajankhanalxz@gmail.com>
2024-10-03 16:35:23 +0545

    conf: garage urls configured

--- garage/urls.py
+++ garage/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
-
+from .views import AuthLoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('client.urls'),name='user'),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/login/', AuthLoginView.as_view(), name='login'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
